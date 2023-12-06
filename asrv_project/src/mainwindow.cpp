4971e8138d9b386f9f078d7439e5818d250ce0aa
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2023-12-06 11:36:36 +0200

    Update mainwindow

--- asrv_project/src/mainwindow.cpp
+++ asrv_project/src/mainwindow.cpp
@@ -1,5 +1,49 @@
 #include "mainwindow.h"
 
+#include <QSplineSeries>
+#include <QChart>
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow{parent}
-{}
+    : QMainWindow(parent)
+{
+    m_spectrogram = new Spectrogram(44100, 44100 * 60, 256, 8192);
+    m_spectrogramWidget = new SpectrogramWidget(m_spectrogram, this);
+
+    setCentralWidget(m_spectrogramWidget);
+
+    resize(1024, 600);
+
+    m_audioThread = new AudioThread(44100, 1024);
+    m_audioThread->start();
+
+    // TODO
+}
+
+MainWindow::~MainWindow() {
+    delete m_spectrogram;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent* event) {
+    qDebug("keyPress %d", event->key());
+
+    switch (event->key()) {
+    case Qt::Key_F1:
+        m_spectrogramWidget->setMinAmpl(0.0001);
+        break;
+    case Qt::Key_F2:
+        m_spectrogramWidget->setMinAmpl(0.001);
+        break;
+    case Qt::Key_F3:
+        m_spectrogramWidget->setMinAmpl(0.01);
+        break;
+    case Qt::Key_F4:
+        m_spectrogramWidget->setMaxAmpl(0.1);
+        break;
+    case Qt::Key_F5:
+        m_spectrogramWidget->setMaxAmpl(1.0);
+        break;
+    case Qt::Key_F6:
+        m_spectrogramWidget->setMaxAmpl(10.0);
+        break;
+    }
+}
--- asrv_project/src/mainwindow.h
+++ asrv_project/src/mainwindow.h
@@ -1,13 +1,33 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "audiothread.h"
+#include "spectrogram.h"
+#include "spectrogramwidget.h"
+
 #include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    AudioThread* m_audioThread = nullptr;
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
+private:
+    Spectrogram* m_spectrogram = nullptr;
+    SpectrogramWidget* m_spectrogramWidget = nullptr;
 
 signals:
 };
